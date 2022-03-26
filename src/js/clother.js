310afa91fcff02f7487ddb68a0d4f1b8d1ceb15b
Deusimar <deusimarmms@outlook.com>
2022-03-25 22:41:08 -0300

    Json com os produtos

---
+++ src/js/clother.js
@@ -0,0 +1,11 @@
+let clotherJson = [
+    {id:1, name:'Roupa 1', img:'./src/img/roupa-1.png', price:20.19, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:2, name:'Roupa 2', img:'./src/img/roupa-2.png', price:18.00, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:3, name:'Roupa 3', img:'./src/img/roupa-3.png', price:17.45, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:4, name:'Roupa 4', img:'./src/img/roupa-4.png', price:19.77, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:5, name:'Roupa 5', img:'./src/img/roupa-5.png', price:21.43, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:6, name:'Roupa 6', img:'./src/img/roupa-6.png', price:18.55, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:7, name:'Roupa 7', img:'./src/img/roupa-7.png', price:22.36, sizes:['P', 'M', 'G'], description:'LOREM'},
+    {id:8, name:'Roupa 8', img:'./src/img/roupa-8.png', price:22.36, sizes:['P', 'M', 'G'], description:'LOREM'},
+
+];
