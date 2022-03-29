85a9bf3ecefe5660eeda749d3297b35045e42181
Deusimar <deusimarmms@outlook.com>
2022-03-29 11:50:38 -0300

    Aplicando correções no formulario

--- src/js/validaForm.js
+++ src/js/validaForm.js
@@ -1,3 +1,6 @@
+let textArea = document.querySelector('.text__area')
+
+
 let Validator = {
     handleSubmit: (event) => {
         event.preventDefault();
@@ -23,6 +26,40 @@ let Validator = {
             form.reset();
         }
     },
+
+    
+    showError: (input, error) => {
+        input.style.borderColor = '#FF0000';
+       
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+       
+
+        
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll('input');
+   
+
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+
+
+        let errorElements = document.querySelectorAll('.error');
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+    },
+
+
+
+
+
     checkInput: (input) => {
         let rules = input.getAttribute('data-rules');
 
@@ -41,6 +78,11 @@ let Validator = {
                             return 'Campo tem que ter pelo menos ' + rDetails[1] + ' caractes';
                         }
                         break;
+                    case 'mintel':
+                        if (input.value.length < rDetails[1]) {
+                            return 'Telefone Incompleto '  ;
+                        }
+                        break;
                     case 'email':
                         if (input.value != '') {
                             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -57,22 +99,22 @@ let Validator = {
                             }
                         }
                         break;
-                    case 'tel':
-                        if (input.value == '') {
+/*                     case 'tel':
+                        if ($('.phone_with_ddd').mask('(00) 0000-0000');) {
                             let regex = /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/;
                             if (!regex.test(input.value.toLowerCase())) {
                                 return 'Numero de telefone não é valido';
                             }
                         }
-                        break;
-                    case 'cep':
+                        break; */
+/*                     case 'cep':
                         if (input.value === '') {
                             let regex = /^([\d]{2})\.*([\d]{3})-*([\d]{3})/;
                             if (!regex.test(input.value.toLowerCase())) {
                                 return 'Campo tem que ter pelo menos ' + rDetails[1] + ' caractes com o "-"';
                             }
                         }
-                        break;
+                        break; */
                 }
             }
         }
@@ -100,7 +142,15 @@ let Validator = {
         }
     }
 };
+/* Jquery mask */
 
+$('.cep').mask('00000-000');
+$('.phone_with_ddd').mask('(00) 0000-00000');
+$('.alpha-no-spaces').mask("A", {
+	translation: {
+		"A": { pattern: /[\w@\-.+]/, recursive: true }
+	}
+});
 let form = document.querySelector('.Validator');
 form.addEventListener('submit', Validator.handleSubmit);
 
